e68cfc91b5eb7f5bf28be610e61f5e57afbe2d08
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 09:07:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ function sayHiToGrandma(string2){
   if (string2===string2.toLowerCase()){
   console.log(string2+' Oom Lower')
   console.log(string2.toLowerCase())
-  if (string2.toUpperCase===string2){console.log('Mis-match')}
+  if (string2.toUpperCase()===string2){console.log('Mis-match')}
   console.log(string2+' Oom Lower')
-
   return 'I can\'t hear you!'
   }
   else if 
